7751d6b833a2f65c4286a41987fdd6e17a75426b
farzin89 <farzin_ahmadi90@yahoo.com>
2022-10-16 14:10:54 +0200

    store all the point that we click on

--- AngleMeasurment.py
+++ AngleMeasurment.py
@@ -3,13 +3,21 @@ import math
 
 path = 'test.JPG'
 img = cv2.imread(path)
-
+pointsList = []
 # get the mouse point
 def mousePoints(event,x,y,flags,params):
     if event == cv2.EVENT_LBUTTONDOWN:
-        print(x,y)
+        # store all the point that we click on
+        cv2.circle(img,(x,y),5,(0,0,255),cv2.FILLED)
+        pointsList.append([x,y])
+        print(pointsList)
+        #print(x,y)
+# store all the point that we click on
+while True:
+    cv2.imshow('Image',img)
+    cv2.setMouseCallback('Image',mousePoints )
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        pointsList = []
+        img = cv2.imread(path)
 
-cv2.imshow('Image',img)
-cv2.setMouseCallback('Image',mousePoints )
-cv2.waitKey(0)
 
