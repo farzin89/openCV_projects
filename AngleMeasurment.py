a42a6bbe7b3a19ac58edf34bb727d747a82654a4
farzin89 <farzin_ahmadi90@yahoo.com>
2022-10-16 15:05:43 +0200

    calculate gradian and get the angle

--- AngleMeasurment.py
+++ AngleMeasurment.py
@@ -10,10 +10,27 @@ def mousePoints(event,x,y,flags,params):
         # store all the point that we click on
         cv2.circle(img,(x,y),5,(0,0,255),cv2.FILLED)
         pointsList.append([x,y])
-        print(pointsList)
+        #print(pointsList)
+def gradient(pt1,pt2):
+    return(pt2[1] - pt1[1])/(pt2[0]-pt1[0])
+
         #print(x,y)
-# store all the point that we click on
+def getAngle(pointsList):
+    pt1,pt2,pt3 = pointsList[-3:]
+    m1= gradient(pt1,pt2)
+    m2 = gradient(pt1,pt3)
+    angR = math.atan((m2 - m1)/(1+(m2*m1)))
+    angD = round(math.degrees(angR))
+    print(angD)
+
+
+
+
+
 while True:
+
+    if len(pointsList) % 3 == 0 and len(pointsList) !=0 :
+        getAngle(pointsList)
     cv2.imshow('Image',img)
     cv2.setMouseCallback('Image',mousePoints )
     if cv2.waitKey(1) & 0xFF == ord('q'):
