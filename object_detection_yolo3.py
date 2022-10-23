0556ef0c2088b96d80dc70a55d0f9b7b0b6e3c82
farzin89 <farzin_ahmadi90@yahoo.com>
2022-10-23 09:21:01 +0200

    define constants and load class names

--- object_detection_yolo3.py
+++ object_detection_yolo3.py
@@ -1,1 +1,16 @@
 
+import os
+
+#define constants
+
+model_cfg_path = os.path.join("yolov3.cfg")
+model_weight_path = os.path.join("")
+class_name_path = os.path.join("class.names")
+
+img_path = "pexels-diana-huggins-615369.jpg"
+
+# load class names
+
+with open (class_name_path,'r') as f:
+    class_names =(j[:-1] for j in f.readlines() if len(j)>2)
+    f.close()
