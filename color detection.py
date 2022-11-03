6d662a62fd314f2411a038871416f0cfe84edc2f
farzin89 <farzin_ahmadi90@yahoo.com>
2022-11-03 09:14:43 +0100

    create mask and bbox

--- color detection.py
+++ color detection.py
@@ -1,11 +1,26 @@
 import cv2
 import numpy as np
-from util2 import get_limits 
+from util2 import get_limits
+from PIL import Image
+
+yellow = [0,255,255] # yellow in BGR colorspace
 cap = cv2.VideoCapture(0)
 while True:
     ret,frame = cap.read()
 
     hsvImg= cv2.cvtColor(frame,cv2.COLOR_BGR2HSV)
+
+    lowerLimit,upperLimit=get_limits(color=yellow)
+
+    mask = cv2.inRange(hsvImg,lowerLimit,upperLimit)
+
+    mask_= Image.fromarray(mask)
+    bbox = mask_.getbbox()
+
+    if bbox is not None:
+        x1,y1,x2,y2 = bbox
+
+        cv2.rectangle(frame,(x1,y1),(x2,y2),(0,255,0),5)
 
     cv2.imshow('frame',frame)
 
